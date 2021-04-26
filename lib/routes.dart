0d962ea5ccd308c92a52741b5c536e2b8edef72c
Fabian Christoffel <faberchri@gmail.com>
2021-04-26 16:08:51 +0200

    cleanup

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
-import 'package:tour_log/screens/home/home.dart';
 import 'package:tour_log/screens/tour_detail/tour_detail.dart';
+import 'package:tour_log/screens/tour_overview/tour_overview.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   TourOverview.routeName: (BuildContext context) => TourOverview(),
