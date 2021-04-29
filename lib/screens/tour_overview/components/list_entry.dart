25063f3bfde336bd285301a228f24fd59487ab6e
Fabian Christoffel <faberchri@gmail.com>
2021-04-29 22:06:32 +0200

    Deletion of tours in overview

--- lib/screens/tour_overview/components/list_entry.dart
+++ lib/screens/tour_overview/components/list_entry.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:tour_log/models/tour.dart';
 import 'package:tour_log/models/tour_list.dart';
 import 'package:provider/provider.dart';
+import 'package:tour_log/theme/style.dart';
 
 import '../../../routes.dart';
 
@@ -12,16 +14,30 @@ class TourOverviewListEntry extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        title: Text(this.tourModel.title.isNotEmpty
-            ? this.tourModel.title
-            : 'Neue Tour'),
-        subtitle: Text(this.tourModel.remarks.isNotEmpty
-            ? this.tourModel.remarks
-            : this.tourModel.key.id),
-        onTap: () {
-          context.read<TourListModel>().selectTour(tourModel.key);
-          navigateToDetail(context);
-        });
+    return Slidable(
+        actionPane: SlidableDrawerActionPane(),
+        actionExtentRatio: 0.25,
+        key: ValueKey(tourModel.key),
+        secondaryActions: [
+          IconSlideAction(
+            caption: 'Delete',
+            color: appTheme().errorColor,
+            icon: Icons.delete,
+            onTap: () {
+              context.read<TourListModel>().deleteTour(tourModel.key);
+            },
+          ),
+        ],
+        child: ListTile(
+            title: Text(this.tourModel.title.isNotEmpty
+                ? this.tourModel.title
+                : 'Neue Tour'),
+            subtitle: Text(this.tourModel.remarks.isNotEmpty
+                ? this.tourModel.remarks
+                : this.tourModel.key.id),
+            onTap: () {
+              context.read<TourListModel>().selectTour(tourModel.key);
+              navigateToDetail(context);
+            }));
   }
 }
